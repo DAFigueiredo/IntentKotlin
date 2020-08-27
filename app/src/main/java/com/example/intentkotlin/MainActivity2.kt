fa526e86bb805e78887cc68e75e0518dd8846bbd
Diogo Figueiredo <diogo.alvesfigueiredo@gmail.com>
2020-08-27 16:55:48 +0100

    intents

--- app/src/main/java/com/example/intentkotlin/MainActivity2.kt
+++ app/src/main/java/com/example/intentkotlin/MainActivity2.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 
-    fun changeActivity(view: View) {
-        val intent = Intent(applicationContext, MainActivity2::class.java)
+    fun goBack(view: View) {
+        val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
     }
 }
